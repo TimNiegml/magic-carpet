d7231c13e5b778982964804fbbb3db920f221b2e
TimNiegml <niegaowei@gmail.com>
2026-05-13 02:02:32 +0800

    Update tts.js

--- magic-carpet/api/tts.js
+++ magic-carpet/api/tts.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
         app: {
           appid,           // 数字型 APP ID
           token,           // AccessToken
-          cluster: 'volcano_tts',
+          cluster: 'volcano_mega',
         },
         user: { uid: 'magic-carpet-user' },
         audio: {
